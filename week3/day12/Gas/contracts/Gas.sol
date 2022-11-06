8f51add91deb7b199132a36fb1e737933bb38e17
daksb <daksbala@gmail.com>
2022-11-06 08:35:13 -0500

    more changes to optimize gas

--- week3/day12/Gas/contracts/Gas.sol
+++ week3/day12/Gas/contracts/Gas.sol
@@ -195,11 +195,8 @@ contract GasContract is Ownable, Constants {
         payment.recipientName = _name;
         payment.paymentID = ++paymentCounter;
         payments[senderOfTx].push(payment);
-        bool[] memory status = new bool[](tradePercent);
-        for (uint256 i = 0; i < tradePercent; i++) {
-            status[i] = true;
-        }
-        return (status[0] == true);
+        return true;
+
     }
 
     function updatePayment(
